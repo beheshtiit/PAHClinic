398a971c5537b87d3f637b24ba38af09ae211577
Ivan Guevara <ivagueba@hotmail.com>
2015-12-08 10:56:42 -0600

    Uploading Original Site

---
+++ WP Site/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+define('FILES_BASE', dirname(__FILE__));
+
+if (file_exists(FILES_BASE.'/wp-admin/defines.php')) {
+    require_once FILES_BASE.'/wp-admin/defines.php';
+}
+
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
